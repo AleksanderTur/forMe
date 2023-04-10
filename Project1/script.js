926192a1f7ef8c20c1cb06811f2d1d1018f71867
Aleksander Tur <oleksander1506@gmail.com>
2023-04-10 21:44:33 +0200

    First commit

---
+++ Project1/script.js
@@ -0,0 +1,54 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayGuesMassage = function (message) {
+  document.querySelector('.guess-message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guessingNumber = Number(document.querySelector('.number-input').value);
+  console.log(guessingNumber);
+
+  //No input
+  if (!guessingNumber) {
+    displayGuesMassage('Введите число!');
+
+    //Player won
+  } else if (guessingNumber === secretNumber) {
+    displayGuesMassage('Правильно!');
+    document.querySelector('body').style.backgroundColor = 'rgb(9, 250, 21)';
+    document.querySelector('.question').style.width = '50rem';
+    document.querySelector('.question').textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guessingNumber !== secretNumber) {
+    if (score > 1) {
+      displayGuesMassage(
+        guessingNumber > secretNumber ? 'Слишком много!' : 'Слишком мало!'
+      );
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayGuesMassage('GAME OVER');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+
+  displayGuesMassage('Начни угадывать');
+  document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.question').textContent = '???';
+  document.querySelector('.number-input').value = '';
+  document.querySelector('.question').style.width = '25rem';
+});
