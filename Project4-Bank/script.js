f3e7b84147953c5cd559e0f76c0bf56192df3a41
Aleksander Tur <oleksander1506@gmail.com>
2023-04-15 20:27:26 +0200

    Fourth commit

---
+++ Project4-Bank/script.js
@@ -0,0 +1,164 @@
+'use strict';
+
+// Simply Bank App
+
+const account1 = {
+  userName: 'Cecil Ireland',
+  transactions: [500, 250, -300, 5000, -850, -110, -170, 1100],
+  interest: 1.5,
+  pin: 1111,
+};
+
+const account2 = {
+  userName: 'Amani Salt',
+  transactions: [2000, 6400, -1350, -70, -210, -2000, 5500, -30],
+  interest: 1.3,
+  pin: 2222,
+};
+
+const account3 = {
+  userName: 'Corey Martinez',
+  transactions: [900, -200, 280, 300, -200, 150, 1400, -400],
+  interest: 0.8,
+  pin: 3333,
+};
+
+const account4 = {
+  userName: 'Kamile Searle',
+  transactions: [530, 1300, 500, 40, 190],
+  interest: 1,
+  pin: 4444,
+};
+
+const account5 = {
+  userName: 'Oliver Avila',
+  transactions: [630, 800, 300, 50, 120],
+  interest: 1.1,
+  pin: 5555,
+};
+
+const accounts = [account1, account2, account3, account4, account5];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.total__value--in');
+const labelSumOut = document.querySelector('.total__value--out');
+const labelSumInterest = document.querySelector('.total__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerTransactions = document.querySelector('.transactions');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+const displayTransactions = function (transactions) {
+  containerTransactions.innerHTML = '';
+  transactions.forEach(function (trans, index) {
+    const transType = trans > 0 ? 'deposit' : 'withdrawal';
+
+    const transactionRow = `
+    <div class="transactions__row">
+          <div class="transactions__type transactions__type--${transType}">
+            ${index + 1} ${transType}
+          </div>
+          <div class="transactions__value">${trans}$</div>
+        </div>
+    `;
+    containerTransactions.insertAdjacentHTML('afterbegin', transactionRow);
+  });
+};
+
+const createNicknames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.nickname = acc.userName
+      .toLocaleLowerCase()
+      .split(' ')
+      .map(word => word[0])
+      .join('');
+  });
+};
+
+createNicknames(accounts);
+// console.log(accounts);
+
+/// спочатку створюємо і перевіряємо функціональність на одному прикладі, і маленькими кроками, а потім переносим це на більшу "скалу"
+// const userName = 'Oliver Avila'; // nickname = 'oa'
+// const nickname = userName
+//   .toLocaleLowerCase()
+//   .split(' ')
+//   .map(word => word[0])
+//   .join('');
+
+// console.log(nickname);
+
+const displayBalance = function (transactions) {
+  const balance = transactions.reduce((acc, trans) => acc + trans, 0);
+  labelBalance.textContent = `${balance}$`;
+};
+
+const displayTotal = function (account) {
+  const depositiesTotal = account.transactions
+    .filter(trans => trans > 0)
+    .reduce((acc, trans) => acc + trans, 0);
+  labelSumIn.textContent = `${depositiesTotal}$`;
+
+  const withdrawalsTotal = account.transactions
+    .filter(trans => trans < 0)
+    .reduce((acc, trans) => acc + trans, 0);
+  labelSumOut.textContent = `${withdrawalsTotal}`;
+
+  const interestTotal = account.transactions
+    .filter(trans => trans > 0)
+    .map(trans => (trans * account.interest) / 100)
+    .filter(interest => interest > 5)
+    .reduce((acc, interest) => acc + interest, 0);
+  labelSumInterest.textContent = `${interestTotal}$`;
+};
+
+let currentAccount;
+
+btnLogin.addEventListener('click', function (e) {
+  e.preventDefault();
+  currentAccount = accounts.find(
+    account => account.nickname === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    console.log('sdf');
+
+    //display UI and welcom message
+    containerApp.style.opacity = 100;
+    labelWelcome.textContent = `Раді Вас бачити, ${
+      currentAccount.userName.split(' ')[0]
+    }!`;
+
+    //clear input
+    inputLoginUsername.value = '';
+    inputLoginPin.value = '';
+    inputLoginPin.blur();
+
+    //display transactions
+    displayTransactions(currentAccount.transactions);
+
+    //display balance
+    displayBalance(currentAccount.transactions);
+
+    //display total
+    displayTotal(currentAccount);
+  }
+});
