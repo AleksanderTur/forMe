2d1d01b303a4028faca39dd84249651af5749d1c
Aleksander Tur <oleksander1506@gmail.com>
2023-05-11 01:18:31 +0200

    add project to repo

---
+++ Project5-SimpleBank/script.js
@@ -0,0 +1,133 @@
+'use strict';
+
+const modalWindow = document.querySelector('.modal-window');
+const overlay = document.querySelector('.overlay');
+const btnCloseModalWindow = document.querySelector('.btn--close-modal-window');
+const btnsOpenModalWindow = document.querySelectorAll(
+  '.btn--show-modal-window'
+);
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabContainer = document.querySelector('.operations__tab-container');
+const tabContents = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+
+///////////////////////////////////////
+/// Modal window
+const openModalWindow = function (e) {
+  e.preventDefault();
+  modalWindow.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModalWindow = function () {
+  modalWindow.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModalWindow.forEach(button =>
+  button.addEventListener('click', openModalWindow)
+);
+
+btnCloseModalWindow.addEventListener('click', closeModalWindow);
+overlay.addEventListener('click', closeModalWindow);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modalWindow.classList.contains('hidden')) {
+    closeModalWindow();
+  }
+});
+
+///Scroll
+btnScrollTo.addEventListener('click', function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' }); //новий підхід
+});
+
+///Smooth page navigation
+///Delegation event
+//1.Додаємо eventListener для загального батька
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  //2.Визначити target елемент
+  if (e.target.classList.contains('nav__link')) {
+    const href = e.target.getAttribute('href');
+    document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+///Переключання вкладок
+tabContainer.addEventListener('click', function (e) {
+  const clickedButton = e.target.closest('.operations__tab');
+  //Guard clause - Пункт охорони
+  if (!clickedButton) return;
+  //Активна вкладка
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  clickedButton.classList.add('operations__tab--active');
+  //Активний контент
+  tabContents.forEach(content =>
+    content.classList.remove('operations__content--active')
+  );
+  document
+    .querySelector(`.operations__content--${clickedButton.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+///Анімація на панелі навігації
+const navLinksHoverAnimation = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const linkOver = e.target;
+    const siblingLinks = linkOver
+      .closest('.nav__links')
+      .querySelectorAll('.nav__link');
+    const logo = linkOver.closest('.nav').querySelector('img');
+    const logoText = linkOver.closest('.nav').querySelector('.nav__text');
+
+    siblingLinks.forEach(el => {
+      if (el !== linkOver) el.style.opacity = this;
+      logo.style.opacity = this;
+      logoText.style.opacity = this;
+    });
+  }
+};
+
+nav.addEventListener('mouseover', navLinksHoverAnimation.bind(0.4));
+
+nav.addEventListener('mouseout', navLinksHoverAnimation.bind(1));
+
+///Sticky navigation - Intersection Observer API
+const navHeight = nav.getBoundingClientRect().height;
+const getStickyNav = function (entries) {
+  const entry = entries[0];
+  if (!entry.isIntersecting) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+};
+
+const headerObserver = new IntersectionObserver(getStickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+
+///Поява частин сайту
+const appearanceSection = function (entries, observer) {
+  const entry = entries[0];
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(appearanceSection, {
+  root: null,
+  threshold: 0.2,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
