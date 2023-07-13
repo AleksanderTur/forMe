798a63fb69f37af17cc64fac6ce831760de2b8c4
Aleksander Tur <oleksander1506@gmail.com>
2023-07-13 12:41:37 +0200

    13.04

--- study/src/App.js
+++ study/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27,
+      position: '',
+    };
+  }
+
+  nextYear = () => {
+    this.setState(state => ({
+      years: state.years + 1,
+    }));
+  };
+
+  commitInputChanges = e => {
+    this.setState({
+      position: e.target.value,
+    });
+  };
+
+  render() {
+    const { name, surname, link } = this.props;
+    const { years, position } = this.state;
+    return (
+      <div>
+        <button onClick={this.nextYear}>+++</button>
+        <h1>
+          My name is {name}, surname - {surname}, age - {years}, position -{' '}
+          {position}.
+        </h1>
+        <a href={link}>My profile</a>
+        <form>
+          <span>Вкажіть посаду</span>
+          <input type="text" onChange={this.commitInputChanges} />
+        </form>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WhoAmI name="John" surname="Smith" link="facebook.com" />
+      <WhoAmI name="Alex" surname="Shepard" link="vk.com" />
     </div>
   );
 }
