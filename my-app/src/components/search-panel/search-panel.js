b95c0007da25c1e7a9a12b33cf67fbd0d0a2925e
Aleksander Tur <oleksander1506@gmail.com>
2023-07-19 11:55:08 +0200

    19.07

--- my-app/src/components/search-panel/search-panel.js
+++ my-app/src/components/search-panel/search-panel.js
@@ -1,13 +1,31 @@
+import { Component } from 'react';
 import './search-panel.css';
 
-const SearchPanel = () => {
-  return (
-    <input
-      type="text"
-      className="form-control search-input"
-      placeholder="Знайти працівника"
-    />
-  );
-};
+class SearchPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+    };
+  }
+
+  onUpdateSearch1 = e => {
+    const term = e.target.value;
+    this.setState({ term });
+    this.props.onUpdateSearch(term);
+  };
+
+  render() {
+    return (
+      <input
+        type="text"
+        className="form-control search-input"
+        placeholder="Знайти працівника"
+        value={this.state.term}
+        onChange={this.onUpdateSearch1}
+      />
+    );
+  }
+}
 
 export default SearchPanel;
